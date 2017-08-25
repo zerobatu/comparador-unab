cd2fac800332b724ed5d19536a2af99f8b47cfd4
Claudio Alvarado <caa.zerobatu@gmail.com>
2017-08-25 15:37:20 -0300

    - agrego configuracion de correo - agrego ERD para generar diagrama de DB

--- Gemfile
+++ Gemfile
@@ -59,6 +59,8 @@ group :development do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
+  gem 'rails-erd'
 end
 
 gem 'rails_12factor', group: :production
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'vast-earth-99344.herokuapp.com', port: 80 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: 'advance.ing.inf@gmail.com',
+    password: '09090909',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
